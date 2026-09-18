791df5695f2fe54c340fc476264bfa145791184c
Said Elias Pereira Jarava <114439145+ElSaias@users.noreply.github.com>
2026-09-17 22:32:25 -0500

    Add files via upload

---
+++ src/alquilervehiculos/Auto.java
@@ -0,0 +1,17 @@
+public class Auto extends Vehiculo {
+
+    
+    public Auto(String marca, String modelo, double tarifaBase) {
+        super(marca, modelo, tarifaBase);
+    }
+
+    
+    @Override
+    public double calcularCostoAlquiler(int dias) {
+        return getTarifaBase() * dias;
+    }
+
+    
+}
+
+
